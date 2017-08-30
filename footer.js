a36b7185c5cf6225cb320ad071c838ab24d0743d
James Graham <jngraham@u.northwestern.edu>
2017-08-30 18:37:55 -0400

    it's a bit fiddly when the website's css and html doesn't match what's in my repo

--- footer.js
+++ footer.js
@@ -4,12 +4,14 @@ document.write('\
     <div class="col-sm-4">\
       <p>&copy; 2017 James Graham</p>\
     </div>\
-    <ul class="col-sm-8 text-right" id="contact">\
+    <div class="col-sm-8 text-right">\
+    <ul id="contact">\
       <li><a href="index.html">Splash Page</a></li>\
       <li><a href="mailto:jgnraham@u.northwestern.edu" target="_blank">Email</a></li>\
       <li><a href="http://www.linkedin.com/in/j-graham" target="_blank">LinkedIn</a></li>\
       <li><a href="http://github.com/jngraham" target="_blank">Github</a></li>\
     </ul>\
+    </div>\
   </div>\
 </footer>\
 \
