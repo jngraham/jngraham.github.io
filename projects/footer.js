315ffc1f026c450a7ab8ca9f7db4c954d9e2eba2
James Graham <jngraham@u.northwestern.edu>
2017-08-31 12:11:09 -0400

    let's try this gagain, force github to build my site

--- projects/footer.js
+++ projects/footer.js
@@ -4,9 +4,7 @@ document.write('\
     <div class="col-sm-4">\
       <p>&copy; 2017 James Graham</p>\
     </div>\
-    \
     <ul class="col-sm-8 text-right" id="contact">\
-\
       <li><a href="../../index.html">Splash Page</a></li>\
       <li><a href="mailto:jgnraham@u.northwestern.edu">Email</a></li>\
       <li><a href="http://www.linkedin.com/in/j-graham">LinkedIn</a></li>\
