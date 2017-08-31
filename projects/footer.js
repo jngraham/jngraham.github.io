70d5c410ab49b0e2fa950f596d9492c9fddcf7de
James Graham <jngraham@u.northwestern.edu>
2017-08-31 11:22:30 -0400

    It was me all along! a mistake in relative URLs

--- projects/footer.js
+++ projects/footer.js
@@ -5,7 +5,7 @@ document.write('\
       <p>&copy; 2017 James Graham</p>\
     </div>\
     <ul class="col-sm-8 text-right" id="contact">\
-      <li><a href="../index.html">Splash Page</a></li>\
+      <li><a href="../../index.html">Splash Page</a></li>\
       <li><a href="mailto:jgnraham@u.northwestern.edu">Email</a></li>\
       <li><a href="http://www.linkedin.com/in/j-graham">LinkedIn</a></li>\
       <li><a href="http://github.com/jngraham">Github</a></li>\
