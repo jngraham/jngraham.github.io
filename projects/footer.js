51a312478cf4efeea497008170a8baf3388e63fa
James Graham <jngraham@u.northwestern.edu>
2017-08-31 12:00:11 -0400

    still no dice

--- projects/footer.js
+++ projects/footer.js
@@ -4,6 +4,7 @@ document.write('\
     <div class="col-sm-4">\
       <p>&copy; 2017 James Graham</p>\
     </div>\
+    \
     <ul class="col-sm-8 text-right" id="contact">\
       <li><a href="../../index.html">Splash Page</a></li>\
       <li><a href="mailto:jgnraham@u.northwestern.edu">Email</a></li>\
