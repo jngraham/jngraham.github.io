0e95dc56757ca9b64b54ba1b349c158114aa83e9
James Graham <jngraham@u.northwestern.edu>
2018-01-09 17:01:13 -0500

    nuked linkedin

--- footer.js
+++ footer.js
@@ -7,7 +7,6 @@ document.write('\
     <div class="col-sm-8 text-right">\
     <ul id="contact">\
       <li style="display:inline"><a href="mailto:jgnraham@u.northwestern.edu" target="_blank">Email</a></li>\
-      <li style="display:inline"><a href="http://www.linkedin.com/in/j-graham" target="_blank">LinkedIn</a></li>\
       <li style="display:inline"><a href="http://github.com/jngraham" target="_blank">Github</a></li>\
     </ul>\
     </div>\
