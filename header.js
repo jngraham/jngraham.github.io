97429c5bdfb844ff68366321b0cd2884090d8e53
jngraham <jngraham@u.northwestern.edu>
2020-09-03 00:18:52 +0100

    this is why you don't push updates when you're live

--- header.js
+++ header.js
@@ -7,7 +7,6 @@ document.write('\
         <h1 class="col-sm-4"><a href="index.html" id="home">james n graham<a></h1>\
           <ul class="col-sm-8 text-right">\
             <li style="display:inline"><a href="projects.html">Projects</a></li>\
-            <!-- <li style="display:inline"><a href="gallery.html">Gallery</a></li>\-->
             <li style="display:inline"><a href="reading-list.html">Reading List</a></li>\
           </ul>\
         </div>\
