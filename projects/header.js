26f5b844d082fe0ca2e67d33ad36238d435d0d9f
James Graham <jamesngraham@MacBook-Pro-6.local>
2021-01-12 10:11:05 +0000

    killed Gallery in project header

--- projects/header.js
+++ projects/header.js
@@ -7,7 +7,6 @@ document.write('\
         <h1 class="col-sm-4"><a href="../../index.html" id="home">james n graham<a></h1>\
           <ul class="col-sm-8 text-right">\
             <li><a href="../../projects.html">Projects</a></li>\
-            <li><a href="../../gallery.html">Gallery</a></li>\
             <li><a href="../../reading-list.html">Reading List</a></li>\
           </ul>\
         </div>\
